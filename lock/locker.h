01f40fd9b85c0c17f5d712da36e78df5491cd5d8
yuqi-mac <42293725+wyqjy@users.noreply.github.com>
2023-02-17 15:19:55 +0800

    Initial commit

---
+++ lock/locker.h
@@ -0,0 +1,13 @@
+#ifndef MYWEBSERVER_LOCKER_H
+#define MYWEBSERVER_LOCKER_H
+
+#include <pthread.h>
+#include <semaphore.h>
+#include <exception>
+
+// 互斥锁
+class locker {
+
+
+};
+#endif //MYWEBSERVER_LOCKER_H
